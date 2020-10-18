3e473016e5b83bfb54409c15731301bb92e0a392
Jim Huang <jserv@biilabs.io>
2020-10-19 05:06:09 +0800

    Annotate main program

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 #include "elf.h"
 #include "state.h"
 
-// enable program trace mode
-static bool g_arg_trace = false;
+/* enable program trace mode */
+static bool opt_trace = false;
 
-// target executable
-static const char *g_arg_program = "a.out";
+/* target executable */
+static const char *opt_prog_name = "a.out";
 
 static riscv_word_t imp_mem_ifetch(struct riscv_t *rv, riscv_word_t addr)
 {
@@ -61,7 +61,6 @@ static void imp_mem_write_b(struct riscv_t *rv,
 
 static void imp_on_ecall(struct riscv_t *rv)
 {
-    // pass to the syscall handler
     syscall_handler(rv);
 }
 
@@ -70,30 +69,27 @@ static void imp_on_ebreak(struct riscv_t *rv)
     rv_halt(rv);
 }
 
-// run the core - printing out an instruction trace
+/* run: printing out an instruction trace */
 static void run_and_trace(struct riscv_t *rv, elf_t *elf)
 {
     const uint32_t cycles_per_step = 1;
 
-    // run until we see the flag that we are done
-    for (; !rv_has_halted(rv);) {
-        // trace execution
+    for (; !rv_has_halted(rv);) { /* run until the flag is done */
+        /* trace execution */
         uint32_t pc = rv_get_pc(rv);
         const char *sym = elf_find_symbol(elf, pc);
         printf("%08x  %s\n", pc, (sym ? sym : ""));
 
-        // step instructions
+        /* step instructions */
         rv_step(rv, cycles_per_step);
     }
 }
 
-// run the core
 static void run(struct riscv_t *rv)
 {
     const uint32_t cycles_per_step = 100;
-    // run until we see the flag that we are done
-    for (; !rv_has_halted(rv);) {
-        // step instructions
+    for (; !rv_has_halted(rv);) { /* run until the flag is done */
+        /* step instructions */
         rv_step(rv, cycles_per_step);
     }
 }
@@ -110,23 +106,23 @@ static void print_usage(const char *filename)
 
 static bool parse_args(int argc, char **args)
 {
-    // parse each argument in turn
+    /* parse each argument in turn */
     for (int i = 1; i < argc; ++i) {
         const char *arg = args[i];
-        // parse flags
+        /* parse flags */
         if (arg[0] == '-') {
             if (!strcmp(arg, "--help"))
                 return false;
             if (!strcmp(arg, "--trace")) {
-                g_arg_trace = true;
+                opt_trace = true;
                 continue;
             }
-            // otherwise, error
+            /* otherwise, error */
             fprintf(stderr, "Unknown argument '%s'\n", arg);
             return false;
         }
-        // set the executable
-        g_arg_program = arg;
+        /* set the executable */
+        opt_prog_name = arg;
     }
 
     return true;
@@ -134,20 +130,19 @@ static bool parse_args(int argc, char **args)
 
 int main(int argc, char **args)
 {
-    // parse the program arguments
     if (!parse_args(argc, args)) {
         print_usage(args[0]);
         return 1;
     }
 
-    // open the ELF file from disk
+    /* open the ELF file from the file system */
     elf_t *elf = elf_new();
-    if (!elf_open(elf, g_arg_program)) {
-        fprintf(stderr, "Unable to open ELF file '%s'\n", g_arg_program);
+    if (!elf_open(elf, opt_prog_name)) {
+        fprintf(stderr, "Unable to open ELF file '%s'\n", opt_prog_name);
         return 1;
     }
 
-    // setup the IO handlers for the runtime
+    /* install the I/O handlers for the RISC-V runtime */
     const struct riscv_io_t io = {
         imp_mem_ifetch,  imp_mem_read_w,  imp_mem_read_s,
         imp_mem_read_b,  imp_mem_write_w, imp_mem_write_s,
@@ -164,32 +159,32 @@ int main(int argc, char **args)
             c_map_insert(state->fd_map, &i, &files[i]);
     }
 
-    // find the start of the heap
+    /* find the start of the heap */
     const struct Elf32_Sym *end;
     if ((end = elf_get_symbol(elf, "_end")))
         state->break_addr = end->st_value;
 
-    // create the runtime
+    /* create the RISC-V runtime */
     struct riscv_t *rv = rv_create(&io, state);
     if (!rv) {
         fprintf(stderr, "Unable to create riscv emulator\n");
         return 1;
     }
 
-    // load the ELF file into our memory abstraction
+    /* load the ELF file into the memory abstraction */
     if (!elf_load(elf, rv, state->mem)) {
         fprintf(stderr, "Unable to load ELF file '%s'\n", args[1]);
         return 1;
     }
 
-    // run based on the specified mode
-    if (g_arg_trace) {
+    /* run based on the specified mode */
+    if (opt_trace) {
         run_and_trace(rv, elf);
     } else {
         run(rv);
     }
 
-    // delete the runtime
+    /* finalize the RISC-V runtime */
     elf_delete(elf);
     rv_delete(rv);
     c_map_delete(state->fd_map);

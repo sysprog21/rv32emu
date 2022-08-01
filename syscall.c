89a86a9409bf5b565390c72385d3a6673354b58f
Jim Huang <jserv.tw@gmail.com>
2022-08-02 02:06:55 +0800

    Use mach_absolute_time() on macOS
    It would slightly reduce POSIX compatibility overhead on macOS.

--- syscall.c
+++ syscall.c
@@ -5,6 +5,18 @@
 
 #include "state.h"
 
+#if defined(__APPLE__)
+#define HAVE_MACH_TIMER
+#include <mach/mach_time.h>
+#elif !defined(_WIN32) && !defined(_WIN64)
+#define HAVE_POSIX_TIMER
+#ifdef CLOCK_MONOTONIC
+#define CLOCKID CLOCK_MONOTONIC
+#else
+#define CLOCKID CLOCK_REALTIME
+#endif
+#endif
+
 enum {
     SYS_getcwd = 17,
     SYS_dup = 23,
@@ -142,17 +154,23 @@ static void syscall_gettimeofday(struct riscv_t *rv)
 
     /* return the clock time */
     if (tv) {
-#if defined(CLOCK_REALTIME) || defined(CLOCK_MONOTONIC)
+#if defined(HAVE_POSIX_TIMER)
         struct timespec t;
-        clock_gettime(
-#ifdef CLOCK_MONOTONIC
-            CLOCK_MONOTONIC,
-#else
-            CLOCK_REALTIME,
-#endif
-            &t);
+        clock_gettime(CLOCKID, &t);
         int32_t tv_sec = t.tv_sec;
         int32_t tv_usec = t.tv_nsec / 1000;
+#elif defined(HAVE_MACH_TIMER)
+        static mach_timebase_info_data_t info;
+        /* If this is the first time we have run, get the timebase.
+         * We can use denom == 0 to indicate that sTimebaseInfo is
+         * uninitialized.
+         */
+        if (info.denom == 0)
+            (void) mach_timebase_info(&info);
+        /* Hope that the multiplication doesn't overflow. */
+        uint64_t nsecs = mach_absolute_time() * info.numer / info.denom;
+        int32_t tv_sec = nsecs / 1e9;
+        int32_t tv_usec = (nsecs / 1e3) - (tv_sec * 1e6);
 #else /* low resolution timer */
         clock_t t = clock();
         int32_t tv_sec = t / CLOCKS_PER_SEC;

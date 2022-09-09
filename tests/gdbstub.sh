954d55c268fc7bccdbf448d0e6fdde9027e6b131
Jim Huang <jserv.tw@gmail.com>
2022-09-09 12:26:43 +0800

    Ensure gdb is available before running GDB stub

--- tests/gdbstub.sh
+++ tests/gdbstub.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
+GDB=
+prefixes=("${CROSS_COMPILE}" "riscv32-unknown-elf" "riscv-none-elf")
+for prefix in "${prefixes[@]}"; do
+    utility=${prefix}-gdb
+    command -v "${utility}" &> /dev/null
+    if [[ $? == 0 ]]; then
+        GDB=${utility}
+    fi
+done
+
+# Check if GDB is available
+if [ -z ${GDB} ]; then
+    exit 1
+fi
+
 build/rv32emu --gdbstub build/puzzle.elf &
 PID=$!
 
+GDB_COMMANDS=
 # Before starting GDB, we should ensure rv32emu is still running.
 if ps -p $PID > /dev/null
 then
     tmpfile=/tmp/rv32emu-gdbstub.$PID
     breakpoint_arr=(0x10700 0x10800 0x10900)
-    GDB_COMMANDS="riscv32-unknown-elf-gdb --batch "
+    GDB_COMMANDS+=" --batch "
     GDB_COMMANDS+="-ex 'file build/puzzle.elf' "
     GDB_COMMANDS+="-ex 'target remote :1234' "
     for t in ${breakpoint_arr[@]}; do
@@ -25,7 +41,7 @@ then
     GDB_COMMANDS+="-ex 'stepi' "
     GDB_COMMANDS+="-ex 'continue' "
 
-    eval ${GDB_COMMANDS} > ${tmpfile}
+    eval ${GDB} ${GDB_COMMANDS} > ${tmpfile}
 
     # check if we stop at the breakpoint
     for i in {1..3}

fc47cb0547a55ce0c82b06d8030a562a7a9cd819
Jim Huang <jserv.tw@gmail.com>
2022-09-09 12:41:35 +0800

    Simplify GDB stub test script

--- tests/gdbstub.sh
+++ tests/gdbstub.sh
@@ -18,44 +18,44 @@ fi
 build/rv32emu --gdbstub build/puzzle.elf &
 PID=$!
 
-GDB_COMMANDS=
 # Before starting GDB, we should ensure rv32emu is still running.
-if ps -p $PID > /dev/null
-then
-    tmpfile=/tmp/rv32emu-gdbstub.$PID
-    breakpoint_arr=(0x10700 0x10800 0x10900)
-    GDB_COMMANDS+=" --batch "
-    GDB_COMMANDS+="-ex 'file build/puzzle.elf' "
-    GDB_COMMANDS+="-ex 'target remote :1234' "
-    for t in ${breakpoint_arr[@]}; do
-        GDB_COMMANDS+="-ex 'break *$t' "
-    done
-    for i in {1..3}; do
-        GDB_COMMANDS+="-ex 'continue' "
-        GDB_COMMANDS+="-ex 'print \$pc' "
-    done
-    for i in {1..3}; do
-        GDB_COMMANDS+="-ex 'del $i' "
-    done
-    GDB_COMMANDS+="-ex 'stepi' "
-    GDB_COMMANDS+="-ex 'stepi' "
-    GDB_COMMANDS+="-ex 'continue' "
-
-    eval ${GDB} ${GDB_COMMANDS} > ${tmpfile}
-
-    # check if we stop at the breakpoint
-    for i in {1..3}
-    do
-        expected=$(grep -rw "Breakpoint ${i} at" ${tmpfile} | awk {'print $4'})
-        ans=$(grep -r "\$${i} =" ${tmpfile} | awk {'print $5'})
-        if [ "$expected" != "$ans" ]; then
-            # Fail
-            exit 1
-        fi
-    done
-    # Pass and wait
-    exit 0
-    wait $PID
+if ! ps -p $PID > /dev/null; then
+    exit 1
 fi
 
-exit 1
+OPTS=
+tmpfile=/tmp/rv32emu-gdbstub.$PID
+breakpoints=(0x10700 0x10800 0x10900)
+bkpt_count=${#breakpoints[@]}
+OPTS+=" --batch "
+OPTS+="-ex 'file build/puzzle.elf' "
+OPTS+="-ex 'target remote :1234' "
+for t in ${breakpoints[@]}; do
+    OPTS+="-ex 'break *$t' "
+done
+for ((i=1; i<=$bkpt_count; i++ )); do 
+    OPTS+="-ex 'continue' "
+    OPTS+="-ex 'print \$pc' "
+done
+for ((i=1; i<=$bkpt_count; i++ )); do
+    OPTS+="-ex 'del $i' "
+done
+OPTS+="-ex 'stepi' "
+OPTS+="-ex 'stepi' "
+OPTS+="-ex 'continue' "
+
+eval ${GDB} ${OPTS} > ${tmpfile}
+
+# check if we stop at the breakpoint
+for ((i=1; i<=$bkpt_count; i++ )); do
+    expected=$(grep -rw "Breakpoint ${i} at" ${tmpfile} | awk {'print $4'})
+    ans=$(grep -r "\$${i} =" ${tmpfile} | awk {'print $5'})
+    if [ "$expected" != "$ans" ]; then
+        # Fail
+        exit 1
+    fi
+done
+
+# Pass and wait
+wait $PID
+exit 0

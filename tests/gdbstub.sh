8ff0c9bc3cf505240e81b47a796c22276d5003a8
Jim Huang <jserv.tw@gmail.com>
2022-09-10 13:28:29 +0800

    Indent gdbstub test script
    Later, we might follow some consistent style manner such as "Bash Style Guide
    and Coding Standard":
      https://lug.fh-swf.de/vim/vim-bash/StyleGuideShell.en.pdf
    or "Shell Style Guide":
      https://google.github.io/styleguide/shellguide.html

--- tests/gdbstub.sh
+++ tests/gdbstub.sh
@@ -27,32 +27,30 @@ OPTS=
 tmpfile=/tmp/rv32emu-gdbstub.$PID
 breakpoints=(0x10700 0x10800 0x10900)
 bkpt_count=${#breakpoints[@]}
-OPTS+=" --batch "
 OPTS+="-ex 'file build/puzzle.elf' "
 OPTS+="-ex 'target remote :1234' "
 for t in ${breakpoints[@]}; do
     OPTS+="-ex 'break *$t' "
 done
-for ((i=1; i<=$bkpt_count; i++ )); do 
+for ((i = 1; i <= $bkpt_count; i++)); do
     OPTS+="-ex 'continue' "
     OPTS+="-ex 'print \$pc' "
 done
-for ((i=1; i<=$bkpt_count; i++ )); do
+for ((i = 1; i <= $bkpt_count; i++)); do
     OPTS+="-ex 'del $i' "
 done
 OPTS+="-ex 'stepi' "
 OPTS+="-ex 'stepi' "
 OPTS+="-ex 'continue' "
 
-eval ${GDB} ${OPTS} > ${tmpfile}
+eval "${GDB} --batch ${OPTS}" > ${tmpfile}
 
 # check if we stop at the breakpoint
-for ((i=1; i<=$bkpt_count; i++ )); do
-    expected=$(grep -rw "Breakpoint ${i} at" ${tmpfile} | awk {'print $4'})
-    ans=$(grep -r "\$${i} =" ${tmpfile} | awk {'print $5'})
+for ((i = 1; i <= $bkpt_count; i++)); do
+    expected=$(grep -w "Breakpoint ${i} at" ${tmpfile} | awk {'print $4'})
+    ans=$(grep "\$${i} =" ${tmpfile} | awk {'print $5'})
     if [ "$expected" != "$ans" ]; then
-        # Fail
-        exit 1
+        exit 1 # Fail
     fi
 done
 

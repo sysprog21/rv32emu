172c568027a9f04076bd826b302a854a69fe91c4
2011eric <201165r1c@gmail.com>
2022-11-03 21:18:52 +0800

    Move floating point utilities to soft_float.[ch]

--- src/riscv_private.h
+++ src/riscv_private.h
@@ -109,24 +109,6 @@ enum {
     //               ....xxxx....xxxx....xxxx....xxxx
 };
 
-#if RV32_HAS(EXT_F)
-enum {
-    //                    ....xxxx....xxxx....xxxx....xxxx
-    FMASK_SIGN        = 0b10000000000000000000000000000000,
-    FMASK_EXPN        = 0b01111111100000000000000000000000,
-    FMASK_FRAC        = 0b00000000011111111111111111111111,
-    FMASK_QNAN        = 0b00000000010000000000000000000000,
-    //                    ....xxxx....xxxx....xxxx....xxxx
-    FFLAG_MASK        = 0b00000000000000000000000000011111,
-    FFLAG_INVALID_OP  = 0b00000000000000000000000000010000,
-    FFLAG_DIV_BY_ZERO = 0b00000000000000000000000000001000,
-    FFLAG_OVERFLOW    = 0b00000000000000000000000000000100,
-    FFLAG_UNDERFLOW   = 0b00000000000000000000000000000010,
-    FFLAG_INEXACT     = 0b00000000000000000000000000000001,
-    //                    ....xxxx....xxxx....xxxx....xxxx
-    RV_NAN            = 0b01111111110000000000000000000000
-};
-#endif
 /* clang-format off */
 
 enum {
@@ -311,55 +293,7 @@ static inline uint32_t sign_extend_b(const uint32_t x)
     return (int32_t)((int8_t) x);
 }
 
-#if RV32_HAS(EXT_F)
-/* compute the fclass result */
-static inline uint32_t calc_fclass(uint32_t f) 
-{
-  const uint32_t sign = f & FMASK_SIGN;
-  const uint32_t expn = f & FMASK_EXPN;
-  const uint32_t frac = f & FMASK_FRAC;
 
-  /* TODO: optimize with a binary decision tree */
-
-  uint32_t out = 0;
-  /* 0x001    rs1 is -INF */
-  out |= (f == 0xff800000) ? 0x001 : 0;
-  /* 0x002    rs1 is negative normal */
-  out |= (expn && (expn != FMASK_EXPN) && sign) ? 0x002 : 0;
-  /* 0x004    rs1 is negative subnormal */
-  out |= (!expn && frac && sign) ? 0x004 : 0;
-  /* 0x008    rs1 is -0 */
-  out |= (f == 0x80000000) ? 0x008 : 0;
-  /* 0x010    rs1 is +0 */
-  out |= (f == 0x00000000) ? 0x010 : 0;
-  /* 0x020    rs1 is positive subnormal */
-  out |= (!expn && frac && !sign) ? 0x020 : 0;
-  /* 0x040    rs1 is positive normal */
-  out |= (expn && (expn != FMASK_EXPN) && !sign) ? 0x040 : 0;
-  /* 0x080    rs1 is +INF */
-  out |= (expn == FMASK_EXPN && !frac && !sign) ? 0x080 : 0;
-  /* 0x100    rs1 is a signaling NaN */
-  out |= (expn == FMASK_EXPN && frac && !(frac & FMASK_QNAN)) ? 0x100 : 0;
-  /* 0x200    rs1 is a quiet NaN */
-  out |= (expn == FMASK_EXPN && (frac & FMASK_QNAN))? 0x200 : 0;
-
-  return out;
-}
-
-static inline bool is_nan(uint32_t f)
-{
-  const uint32_t expn = f & FMASK_EXPN;
-  const uint32_t frac = f & FMASK_FRAC;
-  return (expn == FMASK_EXPN && frac);
-}
-
-static inline bool is_snan(uint32_t f)
-{
-  const uint32_t expn = f & FMASK_EXPN;
-  const uint32_t frac = f & FMASK_FRAC;
-  return (expn == FMASK_EXPN && frac && !(frac & FMASK_QNAN));
-}
-#endif
 
 #if RV32_HAS(EXT_C)
 enum {
---
+++ src/soft_float.c
@@ -0,0 +1,1 @@
+#include "soft_float.h"

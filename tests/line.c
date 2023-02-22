5b772f3e029e0a16b568b3a631e6c4814e882697
Jim Huang <jserv.tw@gmail.com>
2023-02-23 02:59:35 +0800

    Add license notice

--- tests/line.c
+++ tests/line.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2023 National Cheng Kung University. All rights reserved.
  * Copyright (C) 2017 Milo Yip. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

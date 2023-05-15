a6a901c676b2dff477e9e773122f3c1056f5fcfb
RinHizakura <s921975628@gmail.com>
2023-05-15 23:21:52 +0800

    Add gdbstub test in CICD test

---
+++ .ci/riscv-toolchain-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+. .ci/common.sh
+
+check_platform
+
+mkdir -p toolchain
+
+# GNU Toolchain for RISC-V
+GCC_VER=2023.01.04
+TOOLCHAIN_REPO=https://github.com/riscv-collab/riscv-gnu-toolchain/releases
+
+wget -q \
+    ${TOOLCHAIN_REPO}/download/${GCC_VER}/riscv32-elf-ubuntu-22.04-nightly-${GCC_VER}-nightly.tar.gz -O- \
+| tar -C toolchain -xz

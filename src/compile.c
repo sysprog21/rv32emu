5208f60a408bddb97b5d8d6d062fb4ff6cd07310
YenFuChen <48278026+qwe661234@users.noreply.github.com>
2023-08-14 14:06:35 +0800

    jit: Implement fast R/W operations (#165)
    Considering the modification of memory structure, we need to rewrite the
    fast R/W operations for JIT code generation.
    
    Close #164
    
    Co-authored-by: Allen Yen-Fu Chen <qwe807@andestech.com>

--- src/compile.c
+++ src/compile.c
@@ -178,7 +178,51 @@ RVOP(bltu, { BRNACH_FUNC(uint32_t, <); })
 
 RVOP(bgeu, { BRNACH_FUNC(uint32_t, >=); })
 
+RVOP(lb, {
+    GEN("  addr = rv->X[%u] + %u;\n", ir->rs1, ir->imm);
+    GEN("  rv->X[%u] = sign_extend_b(* (const uint8_t *) (m->mem_base + "
+        "addr));\n",
+        ir->rd);
+})
+
+RVOP(lh, {
+    GEN("  addr = rv->X[%u] + %u;\n", ir->rs1, ir->imm);
+    GEN("  rv->X[%u] = sign_extend_h(* (const uint16_t *) (m->mem_base + "
+        "addr));\n",
+        ir->rd);
+})
+
+#define MEMORY_FUNC(type, IO)                                                \
+    GEN("  addr = rv->X[%u] + %u;\n", ir->rs1, ir->imm);                     \
+    IIF(IO)                                                                  \
+    (GEN("  rv->X[%u] = * (const %s *) (m->mem_base + addr);\n", ir->rd,     \
+         #type),                                                             \
+     GEN("  *(%s *) (m->mem_base + addr) = (%s) rv->X[%u];\n", #type, #type, \
+         ir->rs2));
+
+RVOP(lw, {MEMORY_FUNC(uint32_t, 1)})
+
+RVOP(lbu, {MEMORY_FUNC(uint8_t, 1)})
+
+RVOP(lhu, {MEMORY_FUNC(uint16_t, 1)})
+
+RVOP(sb, {MEMORY_FUNC(uint8_t, 0)})
+
+RVOP(sh, {MEMORY_FUNC(uint16_t, 0)})
+
+RVOP(sw, {MEMORY_FUNC(uint32_t, 0)})
+
 #if RV32_HAS(EXT_C)
+RVOP(clw, {
+    GEN("  addr = rv->X[%u] + %u;\n", ir->rs1, ir->imm);
+    GEN("  rv->X[%u] = * (const uint32_t *) (m->mem_base + addr);\n", ir->rd);
+})
+
+RVOP(csw, {
+    GEN("  addr = rv->X[%u] + %u;\n", ir->rs1, ir->imm);
+    GEN("  *(uint32_t *) (m->mem_base + addr) = rv->X[%u];\n", ir->rs2);
+})
+
 RVOP(cjal, {
     GEN("  rv->X[1] = rv->PC + %u;\n", ir->insn_len);
     UPDATE_PC(ir->imm);
@@ -229,7 +273,18 @@ RVOP(cbnez, {
         GEN("  return true;\n");
     }
 })
+
+RVOP(clwsp, {
+    GEN("addr = rv->X[rv_reg_sp] + %u;\n", ir->imm);
+    GEN("  rv->X[%u] = * (const uint32_t *) (m->mem_base + addr);\n", ir->rd);
+})
+
+RVOP(cswsp, {
+    GEN("addr = rv->X[rv_reg_sp] + %u;\n", ir->imm);
+    GEN("  *(uint32_t *) (m->mem_base + addr) = rv->X[%u];\n", ir->rs2);
+})
 #endif
+
 
 static void gen_fuse1(riscv_t *rv UNUSED,
                       const rv_insn_t *ir,
@@ -275,7 +330,7 @@ static void gen_fuse3(riscv_t *rv UNUSED,
     opcode_fuse_t *fuse = ir->fuse;
     for (int i = 0; i < ir->imm2; i++) {
         GEN("  addr = rv->X[%u] + %u;\n", fuse[i].rs1, fuse[i].imm);
-        GEN("  rv->io.mem_write_w(addr, rv->X[%u]);\n", fuse[i].rs2);
+        GEN("  *(uint32_t *) (m->mem_base + addr) = rv->X[%u];\n", fuse[i].rs2)
     }
     GEN("  rv->PC += ir->imm2 * ir->insn_len;\n");
     GEN("  ir = ir + ir->imm2;\n");
@@ -294,7 +349,8 @@ static void gen_fuse4(riscv_t *rv UNUSED,
     opcode_fuse_t *fuse = ir->fuse;
     for (int i = 0; i < ir->imm2; i++) {
         GEN("  addr = rv->X[%u] + %u;\n", fuse[i].rs1, fuse[i].imm);
-        GEN("  rv->X[%u] = rv->io.mem_read_w(addr);", fuse[i].rd);
+        GEN("  rv->X[%u] = * (const uint32_t *) (m->mem_base + addr);\n",
+            fuse[i].rd);
     }
     GEN("  rv->PC += ir->imm2 * ir->insn_len;\n");
     GEN("  ir = ir + ir->imm2;\n");

b7a7f447860512c50935acbeaae83715a7ed0630
Kuan-Wei Chiu <visitorckw@gmail.com>
2023-09-04 21:05:04 +0800

    Enhance random number generator in test-map.c
    Enhanced randomness and broader range by replacing the rand() function
    with the mt19937 32-bit algorithm in test-map.c. Verified correctness
    by comparing output for the top 10000 generated random numbers with all
    seeds in the [0, 10000) range against GCC 9.4.0 --std=c++11
    implementation of mt19937, confirming identical behavior.

---
+++ tests/map/mt19937_rng.c
@@ -0,0 +1,46 @@
+/*
+ * rv32emu is freely redistributable under the MIT License. See the file
+ * "LICENSE" for information on usage and redistribution of this file.
+ */
+
+#include "mt19937_rng.h"
+
+static uint64_t MT[624];
+static uint64_t index = 0;
+
+void initialize_generator(uint64_t seed)
+{
+    MT[0] = seed;
+    for (int i = 1; i < 624; i++) {
+        MT[i] = 0xffffffffULL &
+                (0x6c078965ULL * (MT[i - 1] ^ (MT[i - 1] >> 30)) + i);
+    }
+}
+
+static void generate_numbers()
+{
+    for (int i = 0; i < 624; i++) {
+        uint64_t y =
+            (MT[i] & 0x80000000ULL) + (MT[(i + 1) % 624] & 0x7fffffffULL);
+        MT[i] = MT[(i + 397) % 624] ^ (y >> 1);
+        if (y % 2 != 0) {
+            MT[i] = MT[i] ^ 0x9908b0dfULL;
+        }
+    }
+}
+
+uint64_t extract_number()
+{
+    if (index == 0) {
+        generate_numbers();
+    }
+
+    uint64_t y = MT[index];
+    y = y ^ (y >> 11);
+    y = y ^ ((y << 7) & 0x9d2c5680ULL);
+    y = y ^ ((y << 15) & 0xefc60000ULL);
+    y = y ^ (y >> 18);
+
+    index = (index + 1) % 624;
+    return y;
+}
---
+++ tests/map/mt19937_rng.h
@@ -0,0 +1,20 @@
+/*
+ * rv32emu is freely redistributable under the MIT License. See the file
+ * "LICENSE" for information on usage and redistribution of this file.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+/*
+ * This is a pseudorandom number generator implemented using the mt19937 32-bit
+ * algorithm. For more information on the mt19937 algorithm, please refer to the
+ * Wikipedia page: https://en.wikipedia.org/wiki/Mersenne_Twister
+ */
+
+/* Initialize the random number generator with a seed */
+void initialize_generator(uint64_t seed);
+
+/* Generate and extract a 32-bit random number */
+uint64_t extract_number();

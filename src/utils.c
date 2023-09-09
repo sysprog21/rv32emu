8cee06c6315853a7f1966daadd819baf7cea362b
Kuan-Wei Chiu <visitorckw@gmail.com>
2023-09-09 10:16:26 +0800

    Refactor time retrieval functions
    This commit refactors time retrieval in utils.c by consolidating
    common logic from rv_gettimeofday and rv_clock_gettime into a new
    get_time_info helper function. This enhances code clarity, reduces
    duplication, and improves maintainability.

--- src/utils.c
+++ src/utils.c
@@ -21,13 +21,13 @@
 #endif
 #endif
 
-void rv_gettimeofday(struct timeval *tv)
+static void get_time_info(int32_t *tv_sec, int32_t *tv_usec)
 {
 #if defined(HAVE_POSIX_TIMER)
     struct timespec t;
     clock_gettime(CLOCKID, &t);
-    int32_t tv_sec = t.tv_sec;
-    int32_t tv_usec = t.tv_nsec / 1000;
+    *tv_sec = t.tv_sec;
+    *tv_usec = t.tv_nsec / 1000;
 #elif defined(HAVE_MACH_TIMER)
     static mach_timebase_info_data_t info;
     /* If it is the first time running, obtain the timebase. Using denom == 0
@@ -37,43 +37,33 @@ void rv_gettimeofday(struct timeval *tv)
         (void) mach_timebase_info(&info);
     /* Hope that the multiplication doesn't overflow. */
     uint64_t nsecs = mach_absolute_time() * info.numer / info.denom;
-    int32_t tv_sec = nsecs / 1e9;
-    int32_t tv_usec = (nsecs / 1e3) - (tv_sec * 1e6);
+    *tv_sec = nsecs / 1e9;
+    *tv_usec = (nsecs / 1e3) - (tv_sec * 1e6);
 #else /* low resolution timer */
     clock_t t = clock();
-    int32_t tv_sec = t / CLOCKS_PER_SEC;
-    int32_t tv_usec = (t % CLOCKS_PER_SEC) * (1000000 / CLOCKS_PER_SEC);
+    *tv_sec = t / CLOCKS_PER_SEC;
+    *tv_usec = (t % CLOCKS_PER_SEC) * (1e6 / CLOCKS_PER_SEC);
 #endif
+}
 
+void rv_gettimeofday(struct timeval *tv)
+{
+    int32_t tv_sec, tv_usec;
+    get_time_info(&tv_sec, &tv_usec);
     tv->tv_sec = tv_sec;
     tv->tv_usec = tv_usec;
 }
 
-/* TODO: clarify newlib internals */
+/*
+ * TODO: Clarify newlib's handling of time units.
+ * It appears that newlib is using millisecond resolution for time manipulation,
+ * while clock_gettime expects nanoseconds in the timespec struct.
+ * Further investigation are needed.
+ */
 void rv_clock_gettime(struct timespec *tp)
 {
-#if defined(HAVE_POSIX_TIMER)
-    struct timespec t;
-    clock_gettime(CLOCKID, &t);
-    int32_t tv_sec = t.tv_sec;
-    int32_t tv_msec = t.tv_nsec / 1e6; /* resolution (ms) */
-#elif defined(HAVE_MACH_TIMER)
-    static mach_timebase_info_data_t info;
-    /* If it is the first time running, obtain the timebase. Using denom == 0
-     * indicates that sTimebaseInfo is uninitialized.
-     */
-    if (info.denom == 0)
-        (void) mach_timebase_info(&info);
-    /* Hope that the multiplication doesn't overflow. */
-    uint64_t nsecs = mach_absolute_time() * info.numer / info.denom;
-    int32_t tv_sec = nsecs / 1e9;
-    int32_t tv_msec = (nsecs / 1e6) - (tv_sec * 1e9);
-#else /* low resolution timer */
-    clock_t t = clock();
-    int32_t tv_sec = t / CLOCKS_PER_SEC;
-    int32_t tv_msec = ((t / 1000) % CLOCKS_PER_SEC) * (1e6 / CLOCKS_PER_SEC);
-#endif
-
+    int32_t tv_sec, tv_usec;
+    get_time_info(&tv_sec, &tv_usec);
     tp->tv_sec = tv_sec;
-    tp->tv_nsec = tv_msec;
+    tp->tv_nsec = tv_usec / 1000; /* Transfer to microseconds */
 }

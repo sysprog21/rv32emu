7b07977866e88d1821bf018dd8da5d7cb145980e
Jim Huang <jserv.tw@gmail.com>
2023-12-22 10:15:55 +0800

    Indent properly

--- src/rv32_template.c
+++ src/rv32_template.c
@@ -215,50 +215,56 @@ RVOP(
     }))
 
 /* clang-format off */
-#define BRANCH_FUNC(type, cond)                                                \
-    const uint32_t pc = PC;                                                    \
-    if ((type) rv->X[ir->rs1] cond (type) rv->X[ir->rs2]) {                     \
-        is_branch_taken = false;                                               \
-        struct rv_insn *untaken = ir->branch_untaken;                          \
-        if (!untaken)                                                          \
-            goto nextop;                                                       \
-        IIF(RV32_HAS(JIT))                                                     \
-        (                                                                      \
-            block_t *block = cache_get(rv->block_cache, PC + 4);               \
-            if (!block) { \
-                ir->branch_untaken = NULL;                                     \
-                goto nextop;                                                   \
-            } untaken = ir->branch_untaken = block->ir_head;                   \
-            if (cache_hot(rv->block_cache, PC + 4)) goto nextop;, );           \
-        PC += 4;                                                               \
-        last_pc = PC;                                                          \
-        MUST_TAIL return untaken->impl(rv, untaken, cycle, PC);                \
-    }                                                                          \
-    is_branch_taken = true;                                                    \
-    PC += ir->imm;                                                             \
-    /* check instruction misaligned */                                         \
-    RV_EXC_MISALIGN_HANDLER(pc, insn, false, 0);                               \
-    struct rv_insn *taken = ir->branch_taken;                                  \
-    if (taken) {                                                               \
-        IIF(RV32_HAS(JIT))                                                     \
-        ({                                                                     \
-            block_t *block = cache_get(rv->block_cache, PC);                   \
-            if (!block) {                                                      \
-                ir->branch_taken = NULL;                                       \
-                goto end_insn;                                                 \
-            }                                                                  \
-            taken = ir->branch_taken = block->ir_head;                         \
-            if (cache_hot(rv->block_cache, PC))                                \
-                goto end_insn;                                                 \
-        }, );                                                                  \
-        last_pc = PC;                                                          \
-        MUST_TAIL return taken->impl(rv, taken, cycle, PC);                    \
-    }                                                                          \
-    end_insn:                                                                  \
-    rv->csr_cycle = cycle;                                                     \
-    rv->PC = PC;                                                               \
-    return true;
+#define BRANCH_COND(type, x, y, cond) \
+    (type) x cond (type) y
 /* clang-format on */
+
+#define BRANCH_FUNC(type, cond)                                    \
+    const uint32_t pc = PC;                                        \
+    if (BRANCH_COND(type, rv->X[ir->rs1], rv->X[ir->rs2], cond)) { \
+        is_branch_taken = false;                                   \
+        struct rv_insn *untaken = ir->branch_untaken;              \
+        if (!untaken)                                              \
+            goto nextop;                                           \
+        IIF(RV32_HAS(JIT))                                         \
+        ({                                                         \
+            block_t *block = cache_get(rv->block_cache, PC + 4);   \
+            if (!block) {                                          \
+                ir->branch_untaken = NULL;                         \
+                goto nextop;                                       \
+            }                                                      \
+            untaken = ir->branch_untaken = block->ir_head;         \
+            if (cache_hot(rv->block_cache, PC + 4))                \
+                goto nextop;                                       \
+        }, );                                                      \
+        PC += 4;                                                   \
+        last_pc = PC;                                              \
+        MUST_TAIL return untaken->impl(rv, untaken, cycle, PC);    \
+    }                                                              \
+    is_branch_taken = true;                                        \
+    PC += ir->imm;                                                 \
+    /* check instruction misaligned */                             \
+    RV_EXC_MISALIGN_HANDLER(pc, insn, false, 0);                   \
+    struct rv_insn *taken = ir->branch_taken;                      \
+    if (taken) {                                                   \
+        IIF(RV32_HAS(JIT))                                         \
+        ({                                                         \
+            block_t *block = cache_get(rv->block_cache, PC);       \
+            if (!block) {                                          \
+                ir->branch_taken = NULL;                           \
+                goto end_insn;                                     \
+            }                                                      \
+            taken = ir->branch_taken = block->ir_head;             \
+            if (cache_hot(rv->block_cache, PC))                    \
+                goto end_insn;                                     \
+        }, );                                                      \
+        last_pc = PC;                                              \
+        MUST_TAIL return taken->impl(rv, taken, cycle, PC);        \
+    }                                                              \
+    end_insn:                                                      \
+    rv->csr_cycle = cycle;                                         \
+    rv->PC = PC;                                                   \
+    return true;
 
 /* In RV32I and RV64I, if the branch is taken, set pc = pc + offset, where
  * offset is a multiple of two; else do nothing. The offset is 13 bits long.

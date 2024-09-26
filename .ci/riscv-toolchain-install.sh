badf8aadaa6fb7b6b384c7e24bf9acb92719974c
Jim Huang <jserv@ccns.ncku.edu.tw>
2024-09-26 09:41:48 +0800

    CI: Bump prebuilt GNU Toolchains

--- .ci/riscv-toolchain-install.sh
+++ .ci/riscv-toolchain-install.sh
@@ -9,12 +9,12 @@ check_platform
 mkdir -p toolchain
 
 if [[ "$#" == "0" ]] || [[ "$1" != "riscv-collab" ]]; then
-    GCC_VER=14.2.0-1
+    GCC_VER=14.2.0-2
     TOOLCHAIN_REPO=https://github.com/xpack-dev-tools/riscv-none-elf-gcc-xpack
     TOOLCHAIN_URL=${TOOLCHAIN_REPO}/releases/download/v${GCC_VER}/xpack-riscv-none-elf-gcc-${GCC_VER}-linux-x64.tar.gz
 else
     UBUNTU_VER=`lsb_release -r | cut -f2`
-    GCC_VER=2024.04.12
+    GCC_VER=2024.09.03
     TOOLCHAIN_REPO=https://github.com/riscv-collab/riscv-gnu-toolchain
     TOOLCHAIN_URL=${TOOLCHAIN_REPO}/releases/download/${GCC_VER}/riscv32-elf-ubuntu-${UBUNTU_VER}-gcc-nightly-${GCC_VER}-nightly.tar.gz
 fi

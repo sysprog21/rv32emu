340d0939b249c46dfa70f9a1fb378ab3a7a99300
Kuan-Wei Chiu <visitorckw@gmail.com>
2024-12-22 22:29:38 +0800

    Add rv_ctz() for efficient count trailing zero operations
    Introduce the rv_ctz() function to compute the count of trailing zeros
    in a 32-bit integer. This function serves as a prerequisite for
    implementing the ctz instruction in the RISC-V Zbb extension.

--- src/common.h
+++ src/common.h
@@ -72,6 +72,43 @@ static inline int rv_clz(uint32_t v)
 }
 #endif
 
+#if defined(_MSC_VER)
+#include <intrin.h>
+static inline int rv_ctz(uint32_t v)
+{
+    /* 0 is considered as undefined behavior */
+    assert(v);
+
+    uint32_t trailing_zero = 0;
+    _BitScanForward(&trailing_zero, v);
+    return trailing_zero;
+}
+#elif defined(__GNUC__) || defined(__clang__)
+static inline int rv_ctz(uint32_t v)
+{
+    /* https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html */
+    /* 0 is considered as undefined behavior */
+    assert(v);
+
+    return __builtin_ctz(v);
+}
+#else /* generic implementation */
+static inline int rv_ctz(uint32_t v)
+{
+    /* 0 is considered as undefined behavior */
+    assert(v);
+
+    /* https://graphics.stanford.edu/~seander/bithacks.html#ZerosOnRightMultLookup
+     */
+
+    static const int mul_debruijn[32] = {
+        0,  1,  28, 2,  29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4,  8,
+        31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6,  11, 5,  10, 9};
+
+    return mul_debruijn[((uint32_t) ((v & -v) * 0x077CB531U)) >> 27];
+}
+#endif
+
 /*
  * Integer log base 2
  *

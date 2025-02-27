878676aff8d1c893b7770ae21a7b68fc8bdf3a98
Meng-Hung Chen <n26130867@gs.ncku.edu.tw>
2025-02-28 02:37:27 +0800

    jit: Suppress compiler error on macOS/arm64
    unused function 'emit_dataproc_3source' [-Werror,-Wunused-function]

--- src/jit.c
+++ src/jit.c
@@ -570,6 +570,7 @@ static void emit_dataproc_2source(struct jit_state *state,
 }
 
 
+#if RV32_HAS(EXT_M)
 /* [ARM-A]: C4.1.67: Data-processing (3 source).  */
 static void emit_dataproc_3source(struct jit_state *state,
                                   bool is64,
@@ -582,6 +583,7 @@ static void emit_dataproc_3source(struct jit_state *state,
     emit_a64(state, sz(is64) | op | (rm << 16) | (ra << 10) | (rn << 5) | rd);
     set_dirty(rd, true);
 }
+#endif
 
 static void update_branch_imm(struct jit_state *state,
                               uint32_t offset,

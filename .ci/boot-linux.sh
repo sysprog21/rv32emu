e2e87ee26385a3602bdbee4629d58bc85e986fa1
ChinYikMing <yikming2222@gmail.com>
2025-03-20 02:15:55 +0800

    CI: Refine Linux boot script for new feature verification
    The Linux boot script previously tested both booting and VirtIO block
    access simultaneously. This commit refines the boot script to test each
    guest Linux feature independently. In addition, a new color (yellow) is
    introduced to clearly indicate which test is currently running in the
    CI, improving debugging capabilities. For future VirtIO device tests or
    other new features, TEST_OPTIONS and EXPECT_CMDS can be easily updated
    to extend the tests, enhancing the overall flexibility of the script.
    
    The VirtIO block device image or loop device is prepared by the
    .ci/boot-linux-prepare.sh script which decouples data and control for
    system emulation validation in .ci/boot-linux.sh. This script can be
    extended in the future to support additional VirtIO devices by preparing
    their associated data prior to Linux boot validation.
    
    The system validation flow is abstracted into a variable called
    BOOT_LINUX_TEST which uses sudo only for data preparation and does not
    use sudo to run .ci/boot-linux.sh, enhancing security when launching
    the system VM.

--- .ci/boot-linux.sh
+++ .ci/boot-linux.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+. .ci/common.sh
+
+check_platform
+
 function cleanup {
     sleep 1
     pkill -9 rv32emu
@@ -8,9 +12,9 @@ function cleanup {
 function ASSERT {
     $*
     local RES=$?
-    if [ $RES -ne 0 ]; then
+    if [ ${RES} -ne 0 ]; then
         echo 'Assert failed: "' $* '"'
-        exit $RES
+        exit ${RES}
     fi
 }
 
@@ -18,61 +22,88 @@ cleanup
 
 ENABLE_VBLK=1
 VBLK_IMG=build/disk.img
-which dd >/dev/null 2>&1 || ENABLE_VBLK=0
-which mkfs.ext4 >/dev/null 2>&1 || which $(brew --prefix e2fsprogs)/sbin/mkfs.ext4 >/dev/null 2>&1 || ENABLE_VBLK=0
-which 7z >/dev/null 2>&1 || ENABLE_VBLK=0
+[ -f "${VBLK_IMG}" ] || ENABLE_VBLK=0
 
 TIMEOUT=50
-OPTS=" -k build/linux-image/Image "
-OPTS+=" -i build/linux-image/rootfs.cpio "
-if [ "$ENABLE_VBLK" -eq "1" ]; then
-    dd if=/dev/zero of=$VBLK_IMG bs=4M count=32
-    mkfs.ext4 $VBLK_IMG || $(brew --prefix e2fsprogs)/sbin/mkfs.ext4 $VBLK_IMG
-    OPTS+=" -x vblk:$VBLK_IMG "
-else
-    printf "Virtio-blk Test...Passed\n"
-fi
-RUN_LINUX="build/rv32emu ${OPTS}"
+OPTS_BASE=" -k build/linux-image/Image"
+OPTS_BASE+=" -i build/linux-image/rootfs.cpio"
 
-if [ "$ENABLE_VBLK" -eq "1" ]; then
-ASSERT expect <<DONE
-set timeout ${TIMEOUT}
-spawn ${RUN_LINUX}
-expect "buildroot login:" { send "root\n" } timeout { exit 1 }
-expect "# " { send "uname -a\n" } timeout { exit 2 }
-expect "riscv32 GNU/Linux" { send "mkdir mnt && mount /dev/vda mnt\n" } timeout { exit 3 }
-expect "# " { send "echo rv32emu > mnt/emu.txt\n" } timeout { exit 3 }
-expect "# " { send "sync\n" } timeout { exit 3 }
-expect "# " { send "umount mnt\n" } timeout { exit 3 }
-expect "# " { send "\x01"; send "x" } timeout { exit 3 }
-DONE
-else
-ASSERT expect <<DONE
-set timeout ${TIMEOUT}
-spawn ${RUN_LINUX}
+TEST_OPTIONS=("base (${OPTS_BASE})")
+EXPECT_CMDS=('
 expect "buildroot login:" { send "root\n" } timeout { exit 1 }
 expect "# " { send "uname -a\n" } timeout { exit 2 }
 expect "riscv32 GNU/Linux" { send "\x01"; send "x" } timeout { exit 3 }
-DONE
-fi
-ret=$?
-cleanup
+')
 
 COLOR_G='\e[32;01m' # Green
 COLOR_R='\e[31;01m' # Red
+COLOR_Y='\e[33;01m' # Yellow
 COLOR_N='\e[0m' # No color
 
 MESSAGES=("${COLOR_G}OK!" \
      "${COLOR_R}Fail to boot" \
      "${COLOR_R}Fail to login" \
      "${COLOR_R}Fail to run commands" \
-     "${COLOR_R}Fail to find emu.txt in $VBLK_IMG"\
+     "${COLOR_R}Fail to find emu.txt in ${VBLK_IMG}"\
 )
 
-printf "\nBoot Linux Test: [ ${MESSAGES[$ret]}${COLOR_N} ]\n"
-if [ "$ENABLE_VBLK" -eq "1" ]; then 
-    7z l $VBLK_IMG | grep emu.txt >/dev/null 2>&1 || ret=4
-    printf "Virtio-blk Test: [ ${MESSAGES[$ret]}${COLOR_N} ]\n"
+if [ "${ENABLE_VBLK}" -eq "1" ]; then
+    # Read-only
+    TEST_OPTIONS+=("${OPTS_BASE} -x vblk:${VBLK_IMG},readonly")
+    EXPECT_CMDS+=('
+    expect "buildroot login:" { send "root\n" } timeout { exit 1 }
+    expect "# " { send "uname -a\n" } timeout { exit 2 }
+    expect "riscv32 GNU/Linux" { send "mkdir mnt && mount /dev/vda mnt\n" } timeout { exit 3 }
+    expect "# " { send "echo rv32emu > mnt/emu.txt\n" } timeout { exit 3 }
+    expect -ex "-sh: can'\''t create mnt/emu.txt: Read-only file system" {} timeout { exit 3 }
+    expect "# " { send "\x01"; send "x" } timeout { exit 3 }
+    ')
+
+    # Read-write using disk image
+    TEST_OPTIONS+=("${OPTS_BASE} -x vblk:${VBLK_IMG}")
+    VBLK_EXPECT_CMDS='
+     expect "buildroot login:" { send "root\n" } timeout { exit 1 }
+     expect "# " { send "uname -a\n" } timeout { exit 2 }
+     expect "riscv32 GNU/Linux" { send "mkdir mnt && mount /dev/vda mnt\n" } timeout { exit 3 }
+     expect "# " { send "echo rv32emu > mnt/emu.txt\n" } timeout { exit 3 }
+     expect "# " { send "sync\n" } timeout { exit 3 }
+     expect "# " { send "umount mnt\n" } timeout { exit 3 }
+     expect "# " { send "\x01"; send "x" } timeout { exit 3 }
+    '
+    EXPECT_CMDS+=("${VBLK_EXPECT_CMDS}")
+
+    # Read-write using /dev/loopx(Linux) or /dev/diskx(Darwin) block device
+    TEST_OPTIONS+=("${OPTS_BASE} -x vblk:${BLK_DEV}")
+    EXPECT_CMDS+=("${VBLK_EXPECT_CMDS}")
 fi
+
+for i in "${!TEST_OPTIONS[@]}"; do
+    printf "${COLOR_Y}===== Test option: ${TEST_OPTIONS[$i]} =====${COLOR_N}\n"
+
+    OPTS="${OPTS_BASE}"
+    # No need to add option when running base test
+    if [[ ! "${TEST_OPTIONS[$i]}" =~ "base" ]]; then
+        OPTS+="${TEST_OPTIONS[$i]}"
+    fi
+    RUN_LINUX="build/rv32emu ${OPTS}"
+
+    ASSERT expect <<-DONE
+	set timeout ${TIMEOUT}
+	spawn ${RUN_LINUX}
+	${EXPECT_CMDS[$i]}
+	DONE
+
+    ret=$?
+    cleanup
+
+    printf "\nBoot Linux Test: [ ${MESSAGES[$ret]}${COLOR_N} ]\n"
+    if [[ "${TEST_OPTIONS[$i]}" =~ vblk ]]; then
+        # read-only test first, so the emu.txt definitely does not exist, skipping the check
+        if [[ ! "${TEST_OPTIONS[$i]}" =~ readonly ]]; then
+            7z l ${VBLK_IMG} | grep emu.txt >/dev/null 2>&1 || ret=4
+        fi
+        printf "Virtio-blk Test: [ ${MESSAGES[$ret]}${COLOR_N} ]\n"
+    fi
+done
 
 exit ${ret}

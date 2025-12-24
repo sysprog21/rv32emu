949bc2193beaf787b7f413605a3f5cd7923aefa4
ChinYikMing <yikming2222@gmail.com>
2025-12-25 03:52:26 +0800

    CI: Add guestOS reboot test suite
    - Store reboot test suite in a new file called reboot.sh.
    - The exit code of the test suite is the accumulated exit code from
      reboot.sh and virtio block test suite in the boot-linux.sh.

---
+++ .ci/reboot.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Get the directory of this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. "${SCRIPT_DIR}/common.sh"
+
+RET=0
+
+# Reboot Tests
+# cold reboot
+TEST_OPTIONS=("${OPTS_BASE} (cold reboot)")
+EXPECT_CMDS=('
+    expect "buildroot login:" { send "root\n" } timeout { exit 1 }
+    expect "# " { send "uname -a\n" } timeout { exit 2 }
+    expect "riscv32 GNU/Linux" { send "reboot\n" } timeout { exit 3 }
+    expect -ex "cold reboot" {} timeout { exit 1 }
+    expect "buildroot login:" { send "root\n" } timeout { exit 1 }
+    expect "# " { send "uname -a\n" } timeout { exit 2 }
+    expect "riscv32 GNU/Linux" { send "\x01"; send "x" } timeout { exit 3 }
+')
+# warm reboot
+TEST_OPTIONS+=("${OPTS_BASE} (warm reboot)")
+EXPECT_CMDS+=('
+    expect "buildroot login:" { send "root\n" } timeout { exit 1 }
+    expect "# " { send "uname -a\n" } timeout { exit 2 }
+    expect "riscv32 GNU/Linux" { send "echo 'warm' > /sys/kernel/reboot/mode && reboot\n" } timeout { exit 3 }
+    expect -ex "warm reboot" {} timeout { exit 1 }
+    expect "buildroot login:" { send "root\n" } timeout { exit 1 }
+    expect "# " { send "uname -a\n" } timeout { exit 2 }
+    expect "riscv32 GNU/Linux" { send "\x01"; send "x" } timeout { exit 3 }
+')
+
+for i in "${!TEST_OPTIONS[@]}"; do
+    printf "${COLOR_Y}===== Test option: ${TEST_OPTIONS[$i]} =====${COLOR_N}\n"
+
+    RUN_LINUX="build/rv32emu ${TEST_OPTIONS[$i]}"
+
+    ASSERT expect <<- DONE
+	set timeout ${TIMEOUT}
+	spawn ${RUN_LINUX}
+	${EXPECT_CMDS[$i]}
+	DONE
+
+    ret=$?
+    RET=$((${RET} + ${ret}))
+    cleanup
+
+    printf "\nBoot Linux Test: [ ${MESSAGES[$ret]}${COLOR_N} ]\n"
+done
+
+exit ${RET}

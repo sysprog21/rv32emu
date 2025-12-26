eea79da678fba52334fd3200f456f0a46a81fcfc
ChinYikMing <yikming2222@gmail.com>
2025-12-27 03:06:02 +0800

    Fix wasm-ld error
    wasm-ld: error: --shared-memory is disallowed by build/emulate.o because it was not compiled with 'atomics' or 'bulk-memory' features.

--- mk/wasm.mk
+++ mk/wasm.mk
@@ -26,6 +26,14 @@ OBJS_EXT += syscall_sdl.o
 LDFLAGS += -pthread
 endif
 
+# setjmp/longjmp needs -pthread
+ifeq ($(call has, SYSTEM), 1)
+ifeq ($(call has, ELF_LOADER), 0)
+$(OUT)/emulate.o: CFLAGS += -pthread
+$(OUT)/syscall.o: CFLAGS += -pthread
+endif
+endif
+
 # More build flags
 CFLAGS_emcc += -sINITIAL_MEMORY=2GB \
 	       -sALLOW_MEMORY_GROWTH \

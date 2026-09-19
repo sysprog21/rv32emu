7daf9423458268af9d19d99f9911398f254cea2d
ChinYikMing <yikming2222@gmail.com>
2026-09-19 17:32:41 +0000

    Boot with gzipped kernel image on WASM platform
    Since WASM platform supports zlib utilities, boot with gzipped
    kernel image will be faster due to smaller network BW requirement.
    
    The ungzipped kernel image is 4.5MiB, and the gzipped one is 2.1MiB
    . If the network BW is unchanged, the latter shows > 2.1x gain.

--- .ci/wasm-build.sh
+++ .ci/wasm-build.sh
@@ -40,7 +40,7 @@ case "${MODE}" in
         cp build/rv32emu.wasm "${STAGE}/"
         # Only emitted for pthread-enabled builds.
         cp build/rv32emu.worker.js "${STAGE}/" || true
-        cp build/linux-image/Image "${STAGE}/"
+        cp build/linux-image/Image.gz "${STAGE}/"
         cp build/linux-image/rootfs.web.cpio "${STAGE}/rootfs.cpio"
         cp build/timidity.tar "${STAGE}/"
         cp build/timidity.tar.gz "${STAGE}/"
